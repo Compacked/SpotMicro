fb5ce7813da746dbffa2aed52ae39655036ddec0
Compacked <79878159+Compacked@users.noreply.github.com>
2021-03-25 12:13:36 -0700

    Added one more sensor

--- DistanceSensor.py
+++ DistanceSensor.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-def distance(measure='cm'):
+def Sensor1(measure='cm'):
   GPIO.setmode(GPIO.BOARD)
   GPIO.setup(12, GPIO.OUT)
   GPIO.setup(16, GPIO.IN)
@@ -18,11 +18,34 @@ def distance(measure='cm'):
   if measure == 'cm':
     distance = tl / 0.000058
   elif measure == 'in':
-    distance] = tl/ 0.000148
+    distance = tl/ 0.000148
   else:
-    print("No unit")
+    print("Invaild Unit")
     distance = None
   
 
 GPIO.cleanup()
+return distance()
+
+def Sensor2(measure='cm'):
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(20, GPIO.OUT)
+  GPIO.setup(21, GPIO.IN)
+
+  GPIO.output(20, False)
+  while GPIO.input(21) == 0:
+  nosig = time.time()
+
+  tl = sig - nosig
+
+  if measure == 'cm':
+    distance == tl / 0.000058
+  elif measure == 'in':
+    distance = tl / 0.000148
+  else:
+    print('Invaild Unit')
+    distance = None
+
+
+GPIO.cleanup():
 return distance()

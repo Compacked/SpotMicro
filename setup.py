ffa2a1c5a12898e1d68ceabc1daad9d27149b6fc
Compacked <79878159+Compacked@users.noreply.github.com>
2021-04-20 13:20:05 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SpotMicro',         
+    version='1.0.0',        
+    packages='SpotMicro',
+    packages_dir={'': 'SpotMicro'},     
+    url='https://CompackedCoding.com',       
+    license='MIT',        
+    author='Eli Gabriel',       
+    author_email='Compacked1911@Gmail.com',
+    description='4 Packages That Make SpotMicro'
+)

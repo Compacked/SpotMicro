1b7051f97c3989ae6278dcab1f81ac25b25b661d
Compacked <79878159+Compacked@users.noreply.github.com>
2021-04-21 07:32:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='SpotMicro',         
     version='1.0.0',        
-    packages='SpotMicro',
+    packages='SpotMain, SpotManualDrive, SpotManualDriveMovements, SpotDistance',
     packages_dir={'': 'SpotMicro'},     
     url='https://CompackedCoding.com',       
     license='MIT',        
